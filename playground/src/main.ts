67b76c1496320a36172ceb2d04e01cf37a3f4a72
windlil <spacesouls@163.com>
2023-08-04 16:18:53 +0800

    fix: fix

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
-import AwwUI from 'aww-ui'
-import 'aww-ui/styles/index.css'
 import App from './App.vue'
 
 const app = createApp(App)
-app.use(AwwUI)
 app.mount('#app')
